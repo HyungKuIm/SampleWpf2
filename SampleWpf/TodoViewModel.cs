2ccb7e6ad4c36afef15a60b4cece445bc5e1662a
HyungKu Im <damansa1@naver.com>
2025-07-16 14:44:42 +0900

    통계 적용

--- SampleWpf/TodoViewModel.cs
+++ SampleWpf/TodoViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,29 @@ namespace SampleWpf
             set => SetProperty(ref _newTodoText, value);
         }
 
+        // 요약용 속성들
+        private int _totalCount;
+        public int TotalCount
+        {
+            get => _totalCount;
+            set => SetProperty(ref _totalCount, value);
+        }
+
+        private int _completedCount;
+        public int CompletedCount
+        {
+            get => _completedCount;
+            set => SetProperty(ref _completedCount, value);
+        }
+
+        private int _uncompletedCount;
+        public int UncompletedCount
+        {
+            get => _uncompletedCount;
+            set => SetProperty(ref _uncompletedCount, value);
+        }
+
+
         public TodoItem SelectedItem
         {
             get => _selectedItem;
@@ -39,6 +64,52 @@ namespace SampleWpf
 
             DeleteCommand = new DelegateCommand<TodoItem>(DeleteTodo);
             //ToggleCompleteCommand = new DelegateCommand<TodoItem>(ToggleComplete);
+
+            TodoItems.CollectionChanged += OnTodoItemsChanged;
+
+            foreach (var item in TodoItems)
+            {
+                SubscribeToItem(item);  
+            }
+        }
+
+        private void SubscribeToItem(TodoItem item)
+        {
+            item.PropertyChanged += OnTodoItemPropertyChanged;
+        }
+
+        private void UnsubscribeToItem(TodoItem item)
+        {
+            item.PropertyChanged -= OnTodoItemPropertyChanged;
+        }
+
+        private void OnTodoItemPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(TodoItem.IsComplete))
+            {
+                RefreshStatistics();
+            }
+        }
+
+        private void OnTodoItemsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (TodoItem item in e.NewItems)
+                {
+                    SubscribeToItem(item);
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                foreach (TodoItem item in e.OldItems)
+                {
+                    UnsubscribeToItem(item);
+                }
+            }
+
+            RefreshStatistics();
         }
 
         private void AddTodo()
@@ -51,6 +122,7 @@ namespace SampleWpf
             };
             TodoItems.Add(newItem);
             NewTodoText = string.Empty;
+            RefreshStatistics();
         }
 
         private bool CanAddTodo()
@@ -67,6 +139,7 @@ namespace SampleWpf
                 return;
             }
             TodoItems.Remove(item);
+            RefreshStatistics();
         }
 
         //private void ToggleComplete(TodoItem item)
@@ -74,5 +147,12 @@ namespace SampleWpf
         //    Debug.WriteLine("ToggleComplete");
         //    item.IsComplete = !item.IsComplete;
         //}
+
+        private void RefreshStatistics()
+        {
+            TotalCount = TodoItems.Count;
+            CompletedCount = TodoItems.Count(x => x.IsComplete);
+            UncompletedCount = TodoItems.Count(x => !x.IsComplete);
+        }
     }
 }
